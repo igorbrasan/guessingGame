b9e13f99dde851a16e5125d93f0f564e11f27f24
Arkroshex <igorbraga.rsantos@gmail.com>
2020-08-31 13:15:31 -0300

    first try

--- game.js
+++ game.js
@@ -1,13 +1,25 @@
 const min = 1;
 const max = 100;
+const magicNumber = getRandomInt(min, max);
+var guess = 200
+
 function getRandomInt(min, max) { //gets a random interger to start the game
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
 }
-const a = getRandomInt(min, max);
+compareValues(magicNumber, guess);
 
-console.log(a);
+console.log(magicNumber);
 
+while (magicNumber != guess) {
+    guess = Number(prompt('Tente adivinhar o número!'))
+
+    if (magicNumber === guess) {
+        alert(`You win! ${guess} is the right number!`)
+        break;
+    }
+}
+}
 
 
