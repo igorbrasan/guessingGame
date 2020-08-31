32d0612cddc94b745934f2cb448854bd64d7e114
Arkroshex <igorbraga.rsantos@gmail.com>
2020-08-31 13:39:41 -0300

    fixed issue in prompt declaration

--- game.js
+++ game.js
@@ -1,25 +1,22 @@
 const min = 1;
 const max = 100;
 const magicNumber = getRandomInt(min, max);
-var guess = 200
 
 function getRandomInt(min, max) { //gets a random interger to start the game
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
 }
-compareValues(magicNumber, guess);
-
 console.log(magicNumber);
 
 while (magicNumber != guess) {
-    guess = Number(prompt('Tente adivinhar o número!'))
+    var guess = Number(prompt('Tente adivinhar o número!'))
 
     if (magicNumber === guess) {
         alert(`You win! ${guess} is the right number!`)
         break;
     }
 }
-}
+
 
 
